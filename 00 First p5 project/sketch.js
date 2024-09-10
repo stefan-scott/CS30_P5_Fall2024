9f2bd6f41f35fe23f26548aed7e5f9f807e746e9
stefan-scott <scottst@spsd.sk.ca>
2024-09-10 15:18:41 -0600

    First Project

---
+++ 00 First p5 project/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Your Name
+// Date
+// One-line description (or more if needed)
+
+// interactive program
+
+function setup() {
+  //this happens exactly once, at the start
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //happens over and over (60fps)
+  //background(220);
+  circle(mouseX, mouseY, 30);
+}
